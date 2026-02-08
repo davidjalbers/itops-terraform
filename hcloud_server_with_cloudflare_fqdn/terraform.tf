6c05b6786401f04e54dd0c712ae0cb6c802afab2
David Julian Albers <dev@davidjalbers.com>
2026-02-08 12:28:59 +0100

    build: bump provider versions

--- hcloud_server_with_cloudflare_fqdn/terraform.tf
+++ hcloud_server_with_cloudflare_fqdn/terraform.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "~> 1.49"
+      version = "~> 1.60"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.49"
+      version = "~> 4.52"
     }
   }
 }
